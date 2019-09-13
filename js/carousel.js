98ee86eeed94cf9fdd7930e784c8444d827abf85
Dmitry Isaenko <isaenkodmitry@gmail.com>
2019-09-13 20:38:16 +0300

    Сделал галерею с превьюшками снизу

--- js/carousel.js
+++ js/carousel.js
@@ -2,9 +2,28 @@ $(document).ready(function(){
     $('.slick-carousel').slick({
     });
 
+
     $('.slick-carousel-media-single').slick({
-      dots: true,
-  infinite: true,
-  centerMode: true
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      fade: false,
+      asNavFor: '.slider-nav'
     });
+    $('.slider-nav').slick({
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      arrows: false,
+      asNavFor: '.slick-carousel-media-single',
+      centerMode: true,
+      centerPadding: '60px',
+      focusOnSelect: true,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+
+
   });
